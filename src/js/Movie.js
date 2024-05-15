ae55b4a17157937da6d61b5c7d3a98aa302b1b0c
myeonghyun-7011 <dhvlftmd7013@naver.com>
2024-05-15 23:18:13 +0900

    2

--- src/js/Movie.js
+++ src/js/Movie.js
@@ -1,9 +1,9 @@
 // Movie.js
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import '../css/Movie.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../css/Movie.css";
 
 const Movie = () => {
   const [movies, setMovies] = useState([]);
@@ -18,22 +18,24 @@ const Movie = () => {
       const yesterday = new Date(today);
       yesterday.setDate(yesterday.getDate() - 1);
       const formattedDate = formatDate(yesterday);
-      const baseUrl = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?';
-      const myKey = 'd0c0ebcd8606c274ad0c8fc093094bc7';
+      const baseUrl =
+        "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?";
+      const myKey = "d0c0ebcd8606c274ad0c8fc093094bc7";
       const url = `${baseUrl}key=${myKey}&targetDt=${formattedDate}`;
 
       const response = await axios.get(url);
       const movieList = response.data.boxOfficeResult.dailyBoxOfficeList;
+
       setMovies(movieList);
     } catch (error) {
-      console.error('Error fetching data: ', error);
+      console.error("Error fetching data: ", error);
     }
   };
 
   const formatDate = (date) => {
     const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
     return `${year}${month}${day}`;
   };
 
@@ -43,15 +45,23 @@ const Movie = () => {
       <div className="movie-container">
         {movies.map((movie, index) => (
           <div key={index} className="movie-item">
-            <p>{movie.rank}위 {movie.movieNm}</p>
-            <a href={`https://search.naver.com/search.naver?query=${encodeURIComponent(movie.movieNm)} 영화`} target="_blank" rel="noopener noreferrer">
+            <p>
+              {movie.rank}위 {movie.movieNm}
+            </p>
+            <a
+              href={`https://search.naver.com/search.naver?query=${encodeURIComponent(
+                movie.movieNm
+              )} 영화`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               영화 정보 보기
             </a>
             &nbsp;
             <Link
               to={{
                 pathname: `/Reservation`,
-                state: { movieNm: movie.movieNm, ticketCount: 100 } // Pass movie name and initial ticket count as state
+                state: { movieNm: movie.movieNm, ticketCount: 100 }, // 영화 이름과 초기 티켓 수를 상태로 전달
               }}
             >
               예매 하러가기
