8f7093e4cc96069369ae0916137fe1e1e0b1fa86
김서우 <seowooo3117@gmail.com>
2024-05-20 15:22:54 +0900

    footer 디자인 및 내용 수정

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Footer = ( props ) => {
-    return (
-        <footer id="footerType" className={`footer__wrap ${props.element}`}>
-            <h2 className="blind">푸터 영역</h2>
-            <div className="footer__inner container">
-                <div className="footer__right">
-                    2022 Webstoryboy. Portfolio is Power<br />All rights reserved.
-                </div>
-            </div>
-        </footer>
-    )
-}
+const Footer = (props) => {
+  return (
+    <footer id="footerType" className={`footer__wrap ${props.element}`}>
+      <h2 className="blind">푸터 영역</h2>
+      <div className="footer__inner container">
+        <div className="footer__right">
+          KAKAO CLOUD ENGINEER 4th FINAL PROJECT <br />
+          ADAPTER ⓒ 2024 All Rights Reserved
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
