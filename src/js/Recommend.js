d5b9fb9e2409502d6692df8a912f047b99f9ffff
김서우 <seowooo3117@gmail.com>
2024-05-22 13:18:36 +0900

    [수정] Recommend 코드 추가 수정

--- src/js/Recommend.js
+++ src/js/Recommend.js
@@ -3,11 +3,9 @@ import "../css/Recommend.css";
 const Recommend = () => {
   return (
     <div className="info">
-      <h2>추천 리스트</h2>
+      <h2>추천 리스트 TOP 10</h2>
       <div className="Recommend-container">
-        <div>
-          <h2>TOP 10</h2>
-        </div>
+        <div>{/*<h2>TOP 10</h2>*/}</div>
         <div className="Recommend-con"></div>
       </div>
     </div>
