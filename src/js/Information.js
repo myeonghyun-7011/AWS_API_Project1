2cea496b11091d7d66aa31a4fc540a700fbca789
myeonghyun-7011 <dhvlftmd7013@naver.com>
2024-05-28 16:36:10 +0900

    [Information] cpu사용량 코드 재수정

--- src/js/Information.js
+++ src/js/Information.js
@@ -1,229 +1,104 @@
-import React, { useEffect, useState } from "react";
-import AWS from "aws-sdk";
-import { useLocation } from 'react-router-dom';
+import React from "react";
 import "../css/Information.css";
 
-const cloudwatch = new AWS.CloudWatch();
-const ec2 = new AWS.EC2();
-const rds = new AWS.RDS();
-
-const Information = () => {
-  const location = useLocation();
-  const { accessKeyId, secretAccessKey, currentRegion } = location.state;
-
-  useEffect(() => {
-    AWS.config.update({
-      accessKeyId: accessKeyId,
-      secretAccessKey: secretAccessKey,
-      region: currentRegion,
-    });
-  }, [accessKeyId, secretAccessKey, currentRegion]);
-
-  const [loading, setLoading] = useState(true);
-  const [cpuData, setCpuData] = useState([]);
-  const [instanceTypes, setInstanceTypes] = useState({});
-  const [rdsData, setRdsData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const ec2InstanceData = await ec2.describeInstances().promise();
-        const ec2Instances = ec2InstanceData.Reservations.flatMap(
-          (reservation) => reservation.Instances
-        ).filter((instance) => instance.State.Name === "running");
-
-        const ec2DataPromises = ec2Instances.map(async (instance) => {
-          const ec2Metrics = await fetchMetrics(
-            "AWS/EC2",
-            "CPUUtilization",
-            instance.InstanceId,
-            "InstanceId"
-          );
-          return {
-            id: instance.InstanceId,
-            name: instance.Tags.find((tag) => tag.Key === "Name").Value,
-            metrics: ec2Metrics,
-          };
-        });
-
-        const ec2Data = await Promise.all(ec2DataPromises);
-        setCpuData(ec2Data);
-
-        const ec2InstanceTypes = {};
-        ec2Instances.forEach((instance) => {
-          ec2InstanceTypes[instance.InstanceId] = instance.InstanceType;
-        });
-        setInstanceTypes(ec2InstanceTypes);
-
-        const rdsInstanceData = await rds.describeDBInstances().promise();
-        const rdsInstances = rdsInstanceData.DBInstances.filter(
-          (instance) => instance.DBInstanceStatus === "available"
-        );
-
-        const rdsDataPromises = rdsInstances.map(async (instance) => {
-          const rdsMetrics = await fetchMetrics(
-            "AWS/RDS",
-            "CPUUtilization",
-            instance.DBInstanceIdentifier,
-            "DBInstanceIdentifier"
-          );
-          return { id: instance.DBInstanceIdentifier, engine: instance.Engine, metrics: rdsMetrics };
-        });
-
-        const rdsData = await Promise.all(rdsDataPromises);
-        setRdsData(rdsData);
-
-        setLoading(false); // Data fetching completed
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        // Show error message to the user
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const fetchMetrics = async (
-    namespace,
-    metricName,
-    instanceId,
-    dimensionName
-  ) => {
-    const startDate = new Date(new Date().getFullYear(), 4, 1); // May 1st
-
-    const params = {
-      Namespace: namespace,
-      MetricName: metricName,
-      Dimensions: [
-        {
-          Name: dimensionName,
-          Value: instanceId,
-        },
-      ],
-      StartTime: startDate,
-      EndTime: new Date(),
-      Period: 3600, // 1-hour interval
-      Statistics: ["Average", "Maximum", "Minimum"],
-    };
-
-    return new Promise((resolve, reject) => {
-      cloudwatch.getMetricStatistics(params, (err, data) => {
-        if (err) reject(err);
-        else resolve(data.Datapoints);
-      });
-    });
+const Information = ({ responseData }) => {
+  // EC2 인스턴스를 필터링하는 함수
+  const filterRunningEC2Instances = (ec2Instances) => {
+    return ec2Instances.filter((instance) => instance.state === "running");
   };
 
-  const calculateStatistics = (data) => {
-    if (!data || data.length === 0) {
-      return { average: "N/A", maximum: "N/A", minimum: "N/A" };
-    }
-
-    let sum = 0;
-    let max = -Infinity;
-    let min = Infinity;
-
-    data.forEach((point) => {
-      if (point && point.Average && point.Maximum && point.Minimum) {
-        sum += point.Average;
-        if (point.Maximum > max) max = point.Maximum;
-        if (point.Minimum < min) min = point.Minimum;
-      }
-    });
-  
-    const average = (sum / data.length).toFixed(2);
-    return {
-      average: average,
-      maximum: max !== -Infinity ? max.toFixed(2) : "N/A",
-      minimum: min !== Infinity ? min.toFixed(2) : "N/A",
-    };
-  };
-
-  const renderMetrics = () => {
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-
-    if (cpuData.length === 0 && rdsData.length === 0) {
-      return <div>No data available</div>;
-    }
-
-    return (
-      <div className="infod">
-        {/* EC2 인스턴스 데이터 */}
-        {cpuData.map((instance) => (
-          <div key={instance.id} className="Information-container">
-            <h2>
-              <span className="highlight">EC2</span> 인스턴스 CPU 사용량 {" "}
-              <br />
-              <span>인스턴스 이름 :</span>{instance.name}
-            </h2>
-            <div className="metric-info">
-              <p className="info-p">
-                평균값 :{" "}
-                <span className="custom-color">
-                  {calculateStatistics(instance.metrics).average}
-                </span>
-              </p>
-              <p className="info-p">
-                최댓값 :{" "}
-                <span className="custom-color">
-                  {calculateStatistics(instance.metrics).maximum}
-                </span>
-              </p>
-              <p className="info-p">
-                최솟값 :{" "}
-                <span className="custom-color">
-                  {calculateStatistics(instance.metrics).minimum}
-                </span>
-              </p>
-            </div>
-            <p className="info-p">
-              EC2 인스턴스 유형 :{" "}
-              <span className="custom-color">{instanceTypes[instance.id]}</span>
-            </p>
-          </div>
-        ))}
-    
-        {/* RDS 인스턴스 데이터 */}
-        {rdsData.map((instance) => (
-          <div key={instance.id} className="Information-container">
-            <h2>
-              <span className="highlight">RDS</span> 인스턴스 CPU 사용량 {" "}
-              <br />
-              <span>DB-인스턴스 이름 :</span>{instance.id}
-              <br />
-              <span>DB-엔진 유형 :</span>{instance.engine}
-               
-            </h2>
-            <div className="metric-info">
-              <p className="info-p">
-                평균값 :{" "}
-                <span className="custom-color">
-                  {calculateStatistics(instance.metrics).average}
-                </span>
-              </p>
-              <p className="info-p">
-                최댓값 :{" "}
-                <span className="custom-color">
-                  {calculateStatistics(instance.metrics).maximum}
-                </span>
-              </p>
-              <p className="info-p">
-                최솟값 :{" "}
-                <span className="custom-color">
-                  {calculateStatistics(instance.metrics).minimum}
-                </span>
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
+  // RDS 인스턴스를 필터링하는 함수
+  const filterAvailableRDSInstances = (rdsInstances) => {
+    return rdsInstances.filter(
+      (instance) => instance.db_instance_status === "available"
     );
-  
   };
 
-  return renderMetrics();
+  return (
+    <div className="infor">
+      <h2>현재 </h2>
+      <div className="Recommend-container">
+        <div className="Recommend-sub-container">
+          <div className="Recommend-section">
+            <p className="Rec-sub-title1">
+              <span className="Rec-sub-title2">EC2</span>Instances
+            </p>
+            {responseData &&
+              responseData.ec2_metrics &&
+              filterRunningEC2Instances(responseData.ec2_metrics).map(
+                (instance, idx) => (
+                  <div key={idx} className="Recommend-instance-group">
+                    <div className="Recommend-instance">
+                      <div className="rec-box">
+                        <p className="id-title">
+                          Instance ID : {instance.instance_id}
+                        </p>
+                        <p>Instance Type : {instance.instance_type}</p>
+                        <p>Instance Name : {instance.instance_name}</p>
+                        <p>State : {instance.state}</p>
+                        <p>Metrics:</p>
+                        <ul>
+                          <li>CPU Utilization:</li>
+                          <ul>
+                            <li>Minimum : {instance.metrics.Minimum}</li>
+                            <li>Maximum : {instance.metrics.Maximum}</li>
+                            <li>Average : {instance.metrics.Average}</li>
+                          </ul>
+                        </ul>
+                      </div>
+                    
+                    </div>
+                  </div>
+                )
+              )}
+          </div>
+        </div>
+        <div className="Recommend-sub-container">
+          <div className="Recommend-section">
+            <p className="Rec-sub-title1">
+              <span className="Rec-sub-title3">RDS</span> Instances
+            </p>
+            {responseData &&
+              responseData.rds_metrics &&
+              filterAvailableRDSInstances(responseData.rds_metrics).map(
+                (instance, idx) => (
+                  <div key={idx} className="Recommend-instance-group">
+                    <div className="Recommend-instance">
+                      <div className="rec-box">
+                        <p className="id-title">
+                          DB Instance Identifier :{" "}
+                          {instance.db_instance_identifier}
+                        </p>
+                        <p>DB Instance Class : {instance.db_instance_class}</p>
+                        <p>Engine : {instance.engine}</p>
+                        <p>Status : {instance.db_instance_status}</p>
+                        <p>Metrics:</p>
+                        <ul>
+                          <li>CPU Utilization:</li>
+                          <ul>
+                            <li>Minimum : {instance.metrics.CPUUtilization.Minimum}</li>
+                            <li>Maximum : {instance.metrics.CPUUtilization.Maximum}</li>
+                            <li>Average : {instance.metrics.CPUUtilization.Average}</li>
+                          </ul>
+                          <li>Database Connections:</li>
+                          <ul>
+                            <li>Minimum : {instance.metrics.DatabaseConnections.Minimum}</li>
+                            <li>Maximum : {instance.metrics.DatabaseConnections.Maximum}</li>
+                            <li>Average : {instance.metrics.DatabaseConnections.Average}</li>
+                          </ul>
+
+                        </ul>
+                      </div>
+
+                    </div>
+                  </div>
+                )
+              )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Information;
