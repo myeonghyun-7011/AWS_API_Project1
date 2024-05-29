9fa7fa60a1e9fcb4bdb797b4a7efa18abe647dbc
김서우 <seowooo3117@gmail.com>
2024-05-29 11:42:34 +0900

    [수정] 추천리스트 페이지 재수정2

--- src/js/Information.js
+++ src/js/Information.js
@@ -33,7 +33,8 @@ const Information = ({ responseData }) => {
                     <div className="infor-instance">
                       <div className="rec-box">
                         <p className="id-title">
-                          EC2 Instance ID : {instance.instance_id}
+                          <span className="Rec-sub-title4">[EC2]</span> Instance
+                          ID : {instance.instance_id}
                         </p>
                         <p>Instance Type : {instance.instance_type}</p>
                         <p>Instance Name : {instance.instance_name}</p>
@@ -64,7 +65,8 @@ const Information = ({ responseData }) => {
                     <div className="infor-instance">
                       <div className="rec-box">
                         <p className="id-title">
-                          DB Instance Identifier :{" "}
+                          <span className="Rec-sub-title4">[RDS]</span> DB
+                          Instance Identifier :{" "}
                           {instance.db_instance_identifier}
                         </p>
                         <p>DB Instance Class : {instance.db_instance_class}</p>
